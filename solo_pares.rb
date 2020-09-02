259f70b41242298e2f70bb1775437d6e4a106fd4
Romina Pineda <romi.macka@gmail.com>
2020-09-02 18:44:34 -0400

    solo_pares.rb ready

--- solo_pares.rb
+++ solo_pares.rb
@@ -3,8 +3,9 @@ n = ARGV[0].to_i
 if n < 0 
     puts "Ingrese un número mayor a 0"
 else
+    puts "Estos son los primeros #{n} pares"
+    n *= 2
     n.times do |i|
     print "#{i} " if i.even?
     end
-    print n if n.even?
 end
